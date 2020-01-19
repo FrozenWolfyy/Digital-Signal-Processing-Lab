e3eaf778f829fbf7100b7a22312c2f47ce7491e0
Pavan Datta Abbineni <apd10@iitbbs.ac.in>
2020-01-19 12:16:16 +0530

    Matlab codes

---
+++ Lab 1/Matlab/firstorderdifference.m
@@ -0,0 +1,22 @@
+a = [1 -1];                                             %first order difference 
+b = [1 0];
+
+num = conv(a, 1);                                          %convolution
+
+[h, w] = freqz(num, 1, 1000);
+[z,p,k] = tf2zp(a, b);
+
+
+fvtool(a,b,'polezero');
+text(real(z)+.1,imag(z),'Zero');
+text(real(p)+.1,imag(p),'Pole');
+
+figure;                                                    %magnitude response of the filter
+
+subplot(2,1,1);plot(w, (abs(h)));
+xlabel('Frequency'); ylabel('Magnitude');axis tight;grid on; hold on;
+title("Magnitude Response")
+
+subplot(2,1,2); plot(w,(angle(h)));                       %frequency response of the filter
+xlabel('Frequency'); ylabel('Angle'); grid on; axis tight;
+title("Phase Response")
