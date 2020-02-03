dfd63bd6b483c3ff155b21d924c68a03882bb3d9
Pavan Datta Abbineni <apd10@iitbbs.ac.in>
2020-02-03 12:08:34 +0530

    -

---
+++ Lab 3/Matlab/DTFT.m
@@ -0,0 +1,20 @@
+clc;clear all; close all;
+t  = 0:1/25:2.96;
+y=xlsread('Data');
+fs = 25;
+n = length(y);
+W1 = DFT_matrix(length(y));
+X = W1*y';
+MX = abs(X);
+k = 1:n;
+f = fs*k/n;
+subplot(4,1,1);plot(t,y);
+subplot(4,1,2);plot(k,MX);axis tight;
+subplot(4,1,3);plot(f,MX);
+
+xr = conj(W1)*X/length(MX);
+xfft = ifft(X);
+subplot(4,1,4);plot(t,xr);fft);
+figure();
+subplot(2,1,1);plot(t,y);
+subplot(2,1,2);plot(t,xfft);
